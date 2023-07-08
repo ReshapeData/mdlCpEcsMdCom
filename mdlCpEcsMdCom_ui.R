9c63611d46ec23dfa5a4cf8aaa21ed364c81fbc4
takewiki <hulilei@takewiki.com.cn>
2023-07-08 19:26:03 +0800

    v1.1

--- mdlCpEcsMdCom_ui.R
+++ mdlCpEcsMdCom_ui.R
@@ -4,14 +4,15 @@
 tabItem(tabName = "mdlCpEcsMdCom",
                     fluidRow(
                       column(width = 12,
-                             tabBox(title ="数据中台模块开发DEMO工作台",width = 12,
+                             tabBox(title ="ECS基础资料工作台",width = 12,
                                     id='tabSet_mdlCpEcsMdCom',height = '300px',
                                     #以上为注册ui组件的内容-----
                                     #begin----
                                     #uiComponent seperated by comma (,)
-                                    mdlDemoUI::dateUI(),
-                                     mdlDemoUI::buttonUI(),
-                                     mdlDemoUI::textUI()
+                                    mdlCpEcsMdUI::materialUI(),
+                                    mdlCpEcsMdUI::customerUI(),
+                                    mdlCpEcsMdUI::supplierUI()
+                                  
                                      
                                     
                                     
